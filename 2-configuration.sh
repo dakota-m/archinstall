f18d9d5422048b029079ae25a388a67192e28553
Dakota Martin <dakota.martin1232@gmail.com>
2023-11-17 17:19:19 -0700

    adding modified archinstall script

---
+++ 2-configuration.sh
@@ -0,0 +1,227 @@
+#!/bin/bash
+
+#   ____             __ _                       _   _
+#  / ___|___  _ __  / _(_) __ _ _   _ _ __ __ _| |_(_) ___  _ __
+# | |   / _ \| '_ \| |_| |/ _` | | | | '__/ _` | __| |/ _ \| '_ \
+# | |__| (_) | | | |  _| | (_| | |_| | | | (_| | |_| | (_) | | | |
+#  \____\___/|_| |_|_| |_|\__, |\__,_|_|  \__,_|\__|_|\___/|_| |_|
+#                         |___/
+# by Dakota Martin (2023)
+# modified from Stephan Raabe
+# ------------------------------------------------------
+clear
+keyboardlayout="us"
+zoneinfo="Americal/Denver"
+hostname="arch"
+username="dakota"
+
+# Check for Nvidia GPU
+if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -iq nvidia; then
+	ISNVIDIA=true
+else
+	ISNVIDIA=false
+fi
+
+# ------------------------------------------------------
+# Set System Time
+# ------------------------------------------------------
+ln -sf /usr/share/zoneinfo/$zoneinfo /etc/localtime
+hwclock --systohc
+
+# ------------------------------------------------------
+# Update reflector
+# ------------------------------------------------------
+echo "Start reflector..."
+reflector -c "United States," -p https -a 3 --sort rate --save /etc/pacman.d/mirrorlist
+
+# ------------------------------------------------------
+# Synchronize mirrors
+# ------------------------------------------------------
+pacman -Syy
+
+# ------------------------------------------------------
+# Install Packages
+# ------------------------------------------------------
+packages=(
+	acpi
+	acpi_call
+	acpid
+	alsa-utils
+	avahi
+	base-devel
+	bash-completion
+	bat
+	bat-extras
+	bluez
+	bluez-utils
+	brightnessctl
+	btop
+	cliphist
+	cups
+	dnsmasq
+	dnsutils
+	dosfstools
+	dialog
+	duf
+	efibootmgr
+	eza
+	firewalld
+	flatpak
+	gtk3
+	gvfs
+	gvfs-smb
+	inetutils
+	inxi
+	ipset
+	jq
+	linux-headers
+	mtools
+	neofetch
+	networkmanager
+	network-manager-applet
+	nfs-utils
+	nss-mdns
+	ntfs-3g
+	openbsd-netcat
+	openssh
+	os-prober
+	pacman-contrib
+	polkit-kde-agent
+	pipewire
+	python-requests
+	qt5-wayland
+	qt5ct
+	qt6-wayland
+	qt6ct
+	qt5-svg
+	qt5-quickcontrols2
+	qt5-graphicaleffects
+	ranger
+	reflector
+	rsync
+	sof-firmware
+	wireplumber
+	wl-clipboard
+	wpa_supplicant
+	xdg-utils
+	xdg-user-dirs
+	xorg
+	xorg-xinit
+	zip
+	unzip
+)
+# might need these
+# pipewire-alsa pipewire-pulse pipewire-jack
+
+grub_boot=(grub grub-btrfs terminus-font)
+
+pacman --noconfirm -S $packages $grub_boot
+
+# Stephan Raabe Version
+# pacman --noconfirm -S grub xdg-desktop-portal-wlr efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call dnsmasq openbsd-netcat ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font exa bat htop ranger zip unzip neofetch duf xorg xorg-xinit xclip grub-btrfs xf86-video-amdgpu xf86-video-nouveau xf86-video-intel xf86-video-qxl brightnessctl pacman-contrib inxi
+
+# ------------------------------------------------------
+# set lang utf8 US
+# ------------------------------------------------------
+echo "en_US.UTF-8 UTF-8" >>/etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >>/etc/locale.conf
+
+# ------------------------------------------------------
+# Set Keyboard
+# ------------------------------------------------------
+echo "FONT=ter-v18n" >>/etc/vconsole.conf
+echo "KEYMAP=$keyboardlayout" >>/etc/vconsole.conf
+
+# ------------------------------------------------------
+# Set hostname and localhost
+# ------------------------------------------------------
+echo "$hostname" >>/etc/hostname
+echo "127.0.0.1 localhost" >>/etc/hosts
+echo "::1       localhost" >>/etc/hosts
+echo "127.0.1.1 $hostname.localdomain $hostname" >>/etc/hosts
+clear
+
+# ------------------------------------------------------
+# Set Root Password
+# ------------------------------------------------------
+echo "Set root password"
+passwd root
+
+# ------------------------------------------------------
+# Add User
+# ------------------------------------------------------
+echo "Add user $username"
+useradd -m -G wheel $username
+passwd $username
+
+# ------------------------------------------------------
+# Enable Services
+# ------------------------------------------------------
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable avahi-daemon
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable firewalld
+systemctl enable acpid
+
+# ------------------------------------------------------
+# Grub installation
+# ------------------------------------------------------
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
+if [[ "$ISNVIDIA" ]]; then
+	sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash nvidia-drm.modeset=1"/' /etc/default/grub
+fi
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# ------------------------------------------------------
+# Add btrfs and setfont to mkinitcpio
+# ------------------------------------------------------
+# Before: BINARIES=()
+# After:  BINARIES=(btrfs setfont)
+if [[ "$ISNVIDIA" ]]; then
+	sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/g' /etc/mkinitcpio.conf
+fi
+sed -i 's/BINARIES=()/BINARIES=(btrfs setfont)/g' /etc/mkinitcpio.conf
+mkinitcpio -p linux
+
+# ------------------------------------------------------
+# Add user to wheel
+# ------------------------------------------------------
+clear
+echo "Uncomment %wheel group in sudoers (around line 85):"
+echo "Before: #%wheel ALL=(ALL:ALL) ALL"
+echo "After:  %wheel ALL=(ALL:ALL) ALL"
+echo ""
+read -p "Open sudoers now?" c
+EDITOR=vim sudo -E visudo
+usermod -aG wheel $username
+
+# ------------------------------------------------------
+# Copy installation scripts to home directory
+# ------------------------------------------------------
+cp /archinstall/3-yay.sh /home/$username
+cp /archinstall/4-zram.sh /home/$username
+cp /archinstall/5-timeshift.sh /home/$username
+cp /archinstall/6-preload.sh /home/$username
+cp /archinstall/snapshot.sh /home/$username
+
+clear
+echo "     _                   "
+echo "  __| | ___  _ __   ___  "
+echo " / _' |/ _ \| '_ \ / _ \ "
+echo "| (_| | (_) | | | |  __/ "
+echo " \__,_|\___/|_| |_|\___| "
+echo "                         "
+echo ""
+echo "Please find the following additional installation scripts in your home directory:"
+echo "- yay AUR helper: 3-yay.sh"
+echo "- zram swap: 4-zram.sh"
+echo "- timeshift snapshot tool: 5-timeshift.sh"
+echo "- preload application cache: 6-preload.sh"
+echo ""
+echo "Please exit & shutdown (shutdown -h now), remove the installation media and start again."
+echo "Important: Activate WIFI after restart with nmtui."
